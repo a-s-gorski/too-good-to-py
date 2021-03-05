18c72bf04b7c150b0b48a0d5472674cf273a48d3
agorski2 <adamsebastiangorski@gmail.com>
2021-03-05 16:24:56 +0100

    preparing gui for android testing

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.textinput import TextInput
 
 
-
 class MainScreen(GridLayout):
     def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
@@ -20,41 +19,57 @@ class MainScreen(GridLayout):
 
         self.login = "default-none"
         self.password = "default-none"
-        self.latitude = None
-        self.longitude = None
-        self.max_price = None
-        self.max_distance = None
-        self.selected_offers = None
-
+        self.latitude = str(52.23)
+        self.longitude = str(21.01)
+        self.max_price = str(100)
+        self.max_distance = str(10)
+        self.selected_offers = []
+        self.searched_patterns = ""
+        self.offer_id_list = [i for i in range(1, 11)]
+        self.distance_list = [None for _ in range(10)]
+        self.shop_list = [None for _ in range(10)]
+        self.price_list = [None for _ in range(10)]
+        self.address_list = [None for _ in range(10)]
 
         self.loginLabel = Label(text="login")
         self.loginTextInput = TextInput(text=self.login)
+
         self.passwordLabel = Label(text="password")
         self.passwordTextInput = TextInput(text=self.password)
         self.loginTGTG = Button(text="login to TgTg")
 
         self.SetParamsLabel = Label(text="set your params")
         self.DisplayParams = Label(text="params display")
+
         self.LatitudeLabel = Label(text="latitude")
         self.LatitudeTextField = TextInput(text="lat")
-        self.LatitudeDisplay = Label(text="not set yet")
+        self.LatitudeDisplay = Label(text=self.latitude)
+
         self.LongitudeLabel = Label(text="longitude")
         self.LongitudeTextFiled = TextInput(text="long")
-        self.LongitudeDisplay = Label(text="not set yet")
+        self.LongitudeDisplay = Label(text=self.longitude)
+
         self.MaxPriceLabel = Label(text="max price")
         self.MaxPriceTextField = TextInput(text="100")
-        self.MaxPriceDisplay = Label(text="not set yet")
+        self.MaxPriceDisplay = Label(text=self.max_price)
+
         self.MaxDistanceLabel = Label(text="max distance(km)")
         self.MaxDistanceTextField = TextInput(text="10.0")
-        self.MaxDistanceDisplay = Label(text="not set yet")
+        self.MaxDistanceDisplay = Label(text=self.max_price)
+
         self.SearchPatternsLabel = Label(text="Search patterns")
         self.SearchPatternsTextField = TextInput(text="pizza, kawa")
-        self.SearchPatternsDisplay = Label(text="notsetyet")
+        self.SearchPatternsDisplay = Label(text=self.searched_patterns)
+
         self.SetParamsButton = Button(text="setParams")
 
+        self.OffersNumberLabel = Label(text="offer_id")
+        self.DistanceLabel = Label(text="distance(km)")
+        self.ShopLabel = Label(text="shop")
+        self.PriceLabel = Label(text="price")
+        self.AddressLabel = Label(text="address")
 
-
-        self.createGui()
+        self.create_gui()
 
     def add_login_widgets(self):
         self.add_widget(self.loginLabel)
@@ -100,18 +115,21 @@ class MainScreen(GridLayout):
         for i in range(2):
             self.add_widget(Label())
 
+    def add_offers_widgets(self):
+        self.add_offers_labels()
 
 
+    def add_offers_labels(self):
+        self.add_widget(self.OffersNumberLabel)
+        self.add_widget(self.DistanceLabel)
+        self.add_widget(self.ShopLabel)
+        self.add_widget(self.PriceLabel)
+        self.add_widget(self.AddressLabel)
 
-
-    def createGui(self):
+    def create_gui(self):
         self.add_login_widgets()
         self.add_param_widgets()
-
-
-
-
-
+        self.add_offers_widgets()
 
 
 class MyApp(App):
